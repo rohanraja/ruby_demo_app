c81c2130e8e6f3e3dcd66511a012fd7d163949fc
rohan1020 <rohanraja9@gmail.com>
2012-12-30 22:18:00 +0530

    After scaffold micropost
    Users database reverted to original format, deleted the test db:migrate
    ruby file

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121230105646) do
+ActiveRecord::Schema.define(:version => 20121230163748) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "passpocode"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
